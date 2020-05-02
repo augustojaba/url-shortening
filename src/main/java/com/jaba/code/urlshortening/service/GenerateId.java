a864b3d40c37c2e6a97bf401d787f536c4cbbd47
Augusto Baltazar <augusto.jaba@gmail.com>
2020-05-02 16:26:04 +0100

    Add log to each node when get the id
    Also, tweaks on kubernetes yaml to fix connection

--- src/main/java/com/jaba/code/urlshortening/service/GenerateId.java
+++ src/main/java/com/jaba/code/urlshortening/service/GenerateId.java
@@ -1,13 +1,17 @@
 package com.jaba.code.urlshortening.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GenerateId {
-
+  private static final Logger LOGGER = LoggerFactory.getLogger(GenerateId.class);
   private static long id = 0L;
 
   public long nextID() {
-    return ++id;
+    long newId = ++id;
+    LOGGER.info("New Id Generator instantiated: {}", newId);
+    return newId;
   }
 }
